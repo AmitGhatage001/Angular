f5738d70e479a76849a91104b49429820ab93871
Amit Ghatage <amitghatage001@gmail.com>
2024-08-19 10:41:04 +0530

    Created dummy-user.ts and outputted dummy users on ui using String interpolation

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -8,5 +10,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  title="User-Component";
+  selectedUser = DUMMY_USERS[randomIndex];
 }
